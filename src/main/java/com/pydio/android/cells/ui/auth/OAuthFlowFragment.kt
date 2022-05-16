d6ef23ac08ad838be8035b2f1994308a31362f40
Bruno Sinou <bruno.sinou@posteo.de>
2022-05-16 16:21:29 +0200

    Rather open account home after relog from the account list

--- src/main/java/com/pydio/android/cells/ui/auth/OAuthFlowFragment.kt
+++ src/main/java/com/pydio/android/cells/ui/auth/OAuthFlowFragment.kt
@@ -43,19 +43,20 @@ class OAuthFlowFragment : Fragment() {
         oAuthVM.accountID.observe(viewLifecycleOwner) { pair ->
             pair?.let {
                 val (accountID, next) = pair
-//                var nextState = CellsApp.instance.getCurrentState()
                 when (next) {
                     AuthService.NEXT_ACTION_TERMINATE -> {} // Do nothing => we return where we launched the auth process
-                    AuthService.NEXT_ACTION_ACCOUNTS -> {
-                        // A priori, we come from the account list and return there, no need
-                        // to change everything, put a log for the time being to be sure
-//                        Log.i(logTag, "Auth success, about to browse to $nextState")
-                        startActivity(Intent(requireActivity(), MainActivity::class.java))
-                    }
+//                    AuthService.NEXT_ACTION_ACCOUNTS -> {
+//                        // A priori, we come from the account list and return there, no need
+//                        // to change everything, put a log for the time being to be sure
+////                        Log.i(logTag, "Auth success, about to browse to $nextState")
+//                        startActivity(Intent(requireActivity(), MainActivity::class.java))
+//                    }
+                    // Not completely we would rather navigate back to the account list, but this is not possible yet
+                    // So we chose to navigate back to the corresponding account "home"
+                    AuthService.NEXT_ACTION_ACCOUNTS,
                     AuthService.NEXT_ACTION_BROWSE -> {
                         // We have registered a new account and want to browse to it
                         var nextState = StateID.fromId(accountID)
-//                        CellsApp.instance.setCurrentState(nextState)
                         val intent = Intent(requireActivity(), MainActivity::class.java)
                         intent.putExtra(AppNames.EXTRA_STATE, accountID)
                         Log.i(logTag, "Auth Successful, navigating to $nextState")
