9d812cee5e55aa5d2b6a9b157e09705fe8f58621
Bruno Sinou <bruno.sinou@posteo.de>
2022-06-15 18:52:15 +0200

    Try to locally compute MD5 (wip)

---
+++ src/main/java/com/pydio/android/cells/utils/FileUtils.kt
@@ -0,0 +1,13 @@
+package com.pydio.android.cells.utils
+
+import java.security.MessageDigest
+
+
+fun String.toMD5(): String {
+    val bytes = MessageDigest.getInstance("MD5").digest(this.toByteArray())
+    return bytes.toHex()
+}
+
+fun ByteArray.toHex(): String {
+    return joinToString("") { "%02x".format(it) }
+}
