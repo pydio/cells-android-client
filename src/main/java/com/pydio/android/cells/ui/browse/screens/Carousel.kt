1d6e0bb814833e5b98ecc4b1efb147926f98e220
Bruno Sinou <bruno.sinou@posteo.de>
2023-03-28 18:46:55 +0200

    Cleaner code and surface variant as background

--- src/main/java/com/pydio/android/cells/ui/browse/screens/Carousel.kt
+++ src/main/java/com/pydio/android/cells/ui/browse/screens/Carousel.kt
@@ -2,29 +2,20 @@ package com.pydio.android.cells.ui.browse.screens
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.lerp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.pydio.android.cells.AppNames
@@ -32,14 +23,13 @@ import com.pydio.android.cells.db.nodes.RTreeNode
 import com.pydio.android.cells.transfer.glide.encodeModel
 import com.pydio.android.cells.ui.browse.models.CarouselVM
 import com.pydio.cells.transport.StateID
-import kotlin.math.absoluteValue
 
 private const val logTag = "Carousel"
 
 @Composable
 fun Carousel(
     initialStateID: StateID,
-    back: () -> Unit,
+//    back: () -> Unit,
     carouselVM: CarouselVM,
 ) {
 
@@ -51,24 +41,6 @@ fun Carousel(
             carouselVM,
             it,
         )
-
-
-//        Scaffold(
-//            topBar = {
-//                TopAppBar(
-//                    title = { Text("Simple Carousel") },
-//                    backgroundColor = MaterialTheme.colors.surface,
-//                )
-//            },
-//            modifier = Modifier.fillMaxSize()
-//        ) { padding ->
-//            HorizontalPagerWithOffsetTransition(
-//                initialStateID = initialStateID,
-//                carouselVM,
-//                it,
-//                Modifier.padding(padding)
-//            )
-//        }
     }
 }
 
@@ -129,13 +101,17 @@ fun HorizontalPagerWithOffsetTransition(
                     GlideImage(
                         model = encodeModel(items[page], AppNames.LOCAL_FILE_TYPE_FILE),
                         contentDescription = "${items[page].name} thumbnail",
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.surfaceVariant),
                     )
                 } else {
                     GlideImage(
                         model = encodeModel(items[page], AppNames.LOCAL_FILE_TYPE_PREVIEW),
                         contentDescription = "${items[page].name} thumbnail",
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.surfaceVariant),
                     )
                     // TODO we lost this in comparison with XML era
 //                        .placeholder(R.drawable.loading_img)
@@ -158,11 +134,8 @@ fun HorizontalPagerWithOffsetTransition(
     }
 }
 
-fun getItemIndex(
-    initialStateID: StateID,
-    items: List<RTreeNode>,
-): Int {
-    var startingState = initialStateID.id
+fun getItemIndex(initialStateID: StateID, items: List<RTreeNode>): Int {
+    val startingState = initialStateID.id
     items.forEachIndexed { i, node ->
         if (node.encodedState == startingState) {
             return i
