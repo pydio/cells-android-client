2b648457051a20a0d6470fd86a228320ccd9b297
Bruno Sinou <bruno.sinou@posteo.de>
2023-05-12 10:38:18 +0200

    Better TextField behaviour for create and rename popups

--- src/main/java/com/pydio/android/cells/ui/core/composables/dialogs/AskForFolderName.kt
+++ src/main/java/com/pydio/android/cells/ui/core/composables/dialogs/AskForFolderName.kt
@@ -1,15 +1,29 @@
 package com.pydio.android.cells.ui.core.composables.dialogs
 
+import android.view.KeyEvent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.SecureFlagPolicy
 import com.pydio.android.cells.R
@@ -27,26 +41,18 @@ fun AskForFolderName(
 
     val updateValue: (String) -> Unit = { folderName.value = it }
 
+    val doCreate: () -> Unit = { createFolderAt(parStateID, folderName.value) }
+
     AlertDialog(
         title = { Text(stringResource(R.string.dialog_create_folder_title)) },
-        text = { AskForNameContent(folderName.value, updateValue) },
+        text = { AskForNameContent(folderName.value, updateValue, doCreate) },
         confirmButton = {
-            TextButton(
-                onClick = {
-                    // Toast.makeText(ctx, "OK button clicked", Toast.LENGTH_LONG).show()
-                    createFolderAt(parStateID, folderName.value)
-                }
-            ) { Text(stringResource(R.string.dialog_create_folder_positive_btn)) }
+            TextButton(onClick = doCreate) { Text(stringResource(R.string.dialog_create_folder_positive_btn)) }
         },
         dismissButton = {
-            TextButton(
-                onClick = {
-                    // Toast.makeText(ctx, "Cancel button clicked!!", Toast.LENGTH_LONG).show()
-                    dismiss()
-                }
-            ) { Text(stringResource(R.string.button_cancel)) }
+            TextButton(onClick = dismiss) { Text(stringResource(R.string.button_cancel)) }
         },
-        onDismissRequest = { dismiss() },
+        onDismissRequest = dismiss,
         properties = DialogProperties(
             dismissOnBackPress = true,
             dismissOnClickOutside = true,
@@ -55,16 +61,62 @@ fun AskForFolderName(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-private fun AskForNameContent(value: String, updateValue: (String) -> Unit) {
+private fun AskForNameContent(
+    value: String,
+    updateValue: (String) -> Unit,
+    doCreate: () -> Unit,
+) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val imeAction = ImeAction.Done
+
+    val onDone: () -> Unit = {
+        doCreate()
+        focusManager.clearFocus()
+        keyboardController?.hide()
+    }
+
+    val keyboardActions = KeyboardActions(
+        onNext = { focusManager.moveFocus(FocusDirection.Down) },
+        onDone = { onDone() }
+    )
+
+    val focusRequester = FocusRequester()
+
+    val modifier = Modifier
+        .focusRequester(focusRequester)
+        .onPreviewKeyEvent {
+            if (it.key == Key.Tab && it.nativeKeyEvent.action == KeyEvent.ACTION_DOWN) {
+                focusManager.moveFocus(FocusDirection.Down)
+                true
+            } else if (it.key == Key.Enter) {
+                onDone()
+                true
+            } else {
+                false
+            }
+        }
+
     Column {
-        TextField(
+        OutlinedTextField(
             value = value,
             onValueChange = { updateValue(it) },
+            modifier = modifier,
+            label = {
+                Text(stringResource(R.string.name_label))
+            },
             supportingText = {
                 Text(stringResource(R.string.dialog_create_folder_support_text))
             },
+            keyboardOptions = KeyboardOptions(imeAction = imeAction),
+            keyboardActions = keyboardActions,
         )
+    }
+
+    LaunchedEffect(key1 = true) {
+        focusRequester.requestFocus()
     }
 }
--- src/main/java/com/pydio/android/cells/ui/core/composables/dialogs/AskForNewName.kt
+++ src/main/java/com/pydio/android/cells/ui/core/composables/dialogs/AskForNewName.kt
@@ -1,16 +1,33 @@
 package com.pydio.android.cells.ui.core.composables.dialogs
 
+import android.view.KeyEvent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.SecureFlagPolicy
 import com.pydio.android.cells.R
@@ -24,31 +41,20 @@ fun AskForNewName(
     rename: (srcID: StateID, name: String) -> Unit,
     dismiss: () -> Unit
 ) {
-    val newName = remember {
-        mutableStateOf(srcID.fileName)
-    }
-
+    val newName = remember { mutableStateOf(srcID.fileName) }
     val updateValue: (String) -> Unit = { newName.value = it }
+    val doRename: () -> Unit = { rename(srcID, newName.value) }
 
     AlertDialog(
         title = {
-            DialogTitle(icon = CellsIcons.Edit, text = stringResource(R.string.rename_dialog_title))
+            DialogTitle(
+                icon = CellsIcons.Edit,
+                text = stringResource(R.string.rename_dialog_title)
+            )
         },
-        text = { AskForNewNameContent(srcID.fileName, newName.value, updateValue) },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    rename(srcID, newName.value)
-                }
-            ) { Text(stringResource(R.string.rename_dialog_confirm_button)) }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    dismiss()
-                }
-            ) { Text(stringResource(R.string.button_cancel)) }
-        },
+        text = { AskForNameContent(srcID.fileName, newName.value, updateValue, doRename) },
+        confirmButton = { TextButton(onClick = doRename) { Text(stringResource(R.string.rename_dialog_confirm_button)) } },
+        dismissButton = { TextButton(onClick = dismiss) { Text(stringResource(R.string.button_cancel)) } },
         onDismissRequest = { dismiss() },
         properties = DialogProperties(
             dismissOnBackPress = true,
@@ -71,3 +77,61 @@ private fun AskForNewNameContent(oldName: String, value: String, updateValue: (S
         )
     }
 }
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun AskForNameContent(
+    oldName: String,
+    value: String,
+    updateValue: (String) -> Unit,
+    doRename: () -> Unit,
+) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val imeAction = ImeAction.Done
+
+    val onDone: () -> Unit = {
+        doRename()
+        focusManager.clearFocus()
+        keyboardController?.hide()
+    }
+
+    val keyboardActions = KeyboardActions(
+        onNext = { focusManager.moveFocus(FocusDirection.Down) },
+        onDone = { onDone() }
+    )
+
+    val focusRequester = FocusRequester()
+
+    val modifier = Modifier
+        .focusRequester(focusRequester)
+        .onPreviewKeyEvent {
+            if (it.key == Key.Tab && it.nativeKeyEvent.action == KeyEvent.ACTION_DOWN) {
+                focusManager.moveFocus(FocusDirection.Down)
+                true
+            } else if (it.key == Key.Enter) {
+                onDone()
+                true
+            } else {
+                false
+            }
+        }
+        .fillMaxWidth()
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = { updateValue(it) },
+        modifier = modifier,
+        label = { Text(stringResource(R.string.name_label)) },
+        supportingText = { Text(stringResource(R.string.rename_dialog_message, oldName)) },
+        keyboardOptions = KeyboardOptions(imeAction = imeAction),
+        keyboardActions = keyboardActions,
+    )
+
+    LaunchedEffect(key1 = true) {
+        focusRequester.requestFocus()
+    }
+}
+
