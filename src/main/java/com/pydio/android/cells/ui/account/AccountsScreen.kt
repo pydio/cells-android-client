d670bc7643fa7e5734a5afee32d5048717f8aa98
Bruno Sinou <bruno.sinou@posteo.de>
2023-10-31 10:59:08 +0100

    Add a confirmation poup before deleting an account

--- src/main/java/com/pydio/android/cells/ui/account/AccountsScreen.kt
+++ src/main/java/com/pydio/android/cells/ui/account/AccountsScreen.kt
@@ -1,10 +1,10 @@
 package com.pydio.android.cells.ui.account
 
+import android.util.Log
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -14,16 +14,26 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import com.pydio.android.cells.AppKeys
 import com.pydio.android.cells.R
 import com.pydio.android.cells.db.accounts.RSessionView
 import com.pydio.android.cells.ui.browse.BrowseDestinations
 import com.pydio.android.cells.ui.core.composables.DefaultTopBar
+import com.pydio.android.cells.ui.core.composables.dialogs.AskForConfirmation
+import com.pydio.android.cells.ui.core.encodeStateForRoute
+import com.pydio.android.cells.ui.core.lazyStateID
 import com.pydio.android.cells.ui.login.LoginDestinations
 import com.pydio.cells.transport.StateID
-import com.pydio.cells.utils.Log
 import kotlinx.coroutines.launch
 
 private const val LOG_TAG = "AccountsScreen.kt"
+private const val ACCOUNT_LIST = "account-list"
+private const val CONFIRM_DELETION_PREFIX = "confirm_deletion"
+private const val CONFIRM_DELETION_ROUTE = "$CONFIRM_DELETION_PREFIX/{${AppKeys.STATE_ID}}"
 
 @Composable
 fun AccountsScreen(
@@ -33,36 +43,67 @@ fun AccountsScreen(
     openDrawer: () -> Unit,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
-    val scope = rememberCoroutineScope()
-    val accounts = accountListVM.sessions.collectAsState(listOf())
 
-    AccountsScreen(
-        isExpandedScreen = isExpandedScreen,
-        accounts = accounts.value,
-        openAccount = {
-            scope.launch {
-                accountListVM.openSession(it)?.let {
-                    Log.i(LOG_TAG, "About to open session for: $it")
-                    navigateTo(BrowseDestinations.Open.createRoute(it.getStateID()))
+    val scope = rememberCoroutineScope()
+    val navController = rememberNavController()
+
+    val accounts = accountListVM.sessions.collectAsState(listOf())
+    NavHost(navController, ACCOUNT_LIST) {
+        composable(ACCOUNT_LIST) {  // Fills the area provided to the NavHost
+
+            AccountsScreen(
+                isExpandedScreen = isExpandedScreen,
+                accounts = accounts.value,
+                openAccount = {
+                    scope.launch {
+                        accountListVM.openSession(it)?.let {
+                            Log.i(LOG_TAG, "About to open session for: $it")
+                            navigateTo(BrowseDestinations.Open.createRoute(it.getStateID()))
+                        }
+                    }
+                },
+                openDrawer = openDrawer,
+                registerNew = {
+                    navigateTo(LoginDestinations.AskUrl.createRoute())
+                },
+                login = { stateID, skipVerify, isLegacy ->
+                    val route = if (isLegacy) {
+                        LoginDestinations.P8Credentials.createRoute(stateID, skipVerify)
+                    } else {
+                        LoginDestinations.LaunchAuthProcessing.createRoute(stateID, skipVerify)
+                    }
+                    navigateTo(route)
+                },
+                logout = { accountListVM.logoutAccount(it) },
+                forget = {
+                    navController.navigate("$CONFIRM_DELETION_PREFIX/${encodeStateForRoute(it)}")
+                },
+                contentPadding = contentPadding,
+            )
+        }
+
+        dialog(CONFIRM_DELETION_ROUTE) { entry ->
+            val stateID = lazyStateID(entry)
+            if (stateID == StateID.NONE) {
+                Log.e(LOG_TAG, "... cannot navigate with no state ID")
+                return@dialog
+            }
+            AskForConfirmation(
+                icon = null,
+                title = stringResource(R.string.confirm_move_to_recycle_title),
+                desc = stringResource(R.string.account_confirm_deletion_desc, stateID),
+                confirm = {
+                    accountListVM.forgetAccount(stateID)
+                    navController.popBackStack(ACCOUNT_LIST, false)
+                    accountListVM.watch()
+
+                },
+                dismiss = {
+                    navController.popBackStack(ACCOUNT_LIST, false)
                 }
-            }
-        },
-        openDrawer = openDrawer,
-        registerNew = {
-            navigateTo(LoginDestinations.AskUrl.createRoute())
-        },
-        login = { stateID, skipVerify, isLegacy ->
-            val route = if (isLegacy) {
-                LoginDestinations.P8Credentials.createRoute(stateID, skipVerify)
-            } else {
-                LoginDestinations.LaunchAuthProcessing.createRoute(stateID, skipVerify)
-            }
-            navigateTo(route)
-        },
-        logout = { accountListVM.logoutAccount(it) },
-        forget = { accountListVM.forgetAccount(it) },
-        contentPadding = contentPadding,
-    )
+            )
+        }
+    }
 }
 
 @Composable
