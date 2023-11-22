c7908f34354b7439f8a01098e7965c04659ed3ae
Bruno Sinou <bruno.sinou@posteo.de>
2023-11-22 10:10:04 +0100

    Make AppState serializable by default

--- src/main/java/com/pydio/android/cells/ui/MainApp.kt
+++ src/main/java/com/pydio/android/cells/ui/MainApp.kt
@@ -7,11 +7,21 @@ import com.pydio.cells.transport.StateID
 // private const val logTag = "MainApp"
 
 class AppState(
-    val stateID: StateID,
+    stateID: StateID,
     val intentID: String?,
     val route: String?,
     val context: String?,
 ) {
+    // We rather rely on the string version of the StateID so that AppState is serializable by default
+    private val stateIDStr: String
+
+    val stateID: StateID
+        get() = StateID.fromId(stateIDStr)
+
+    init {
+        stateIDStr = stateID.id
+    }
+
     companion object {
         val NONE = AppState(
             stateID = StateID.NONE,
